b7ef7f7dbab77f60980b71fa4652c12c66c7804b
scharczynski <scharczynski@gmail.com>
2017-10-24 16:57:47 -0400

    targeted at mk5 and cleaned up

--- change_xml_default.py
+++ change_xml_default.py
@@ -6,27 +6,20 @@ import sys
 
 
 def main():
-    file_location = "/all/opt/ht/mk5/config/system.xml"
-    #file_location = "/home/steve/workspace/misc/system.xml"
+    file_location = "/opt/ht/mk5/config/system.xml"
     new_value = sys.argv[1]
-
-
     time_now = time.time()
     date_now = str(datetime.date.today())
 
-    call(['cp', '/all/opt/ht/mk5/config/system.xml', '/all/opt/ht/mk5/config/system_' + date_now + str(time_now) + '.xml'])
-    #call(['cp', '/home/steve/workspace/misc/system.xml',
-        '/home/steve/workspace/misc/system_' + date_now + str(time_now) + '.xml'])
-    # print '/home/steve/workspace/misc/system_' + date_now + str(time_now) +
-    # '.xml'
+    call(['cp', '/opt/ht/mk5/config/system.xml', '/opt/ht/mk5/config/system_' + date_now + str(time_now) + '.xml'])
+
     change_element(file_location, "channel",
-                "r_stage_temp_actual", "scaleC", new_value)
+                "r_adc1_ain7", "scaleC", new_value)
 
-    print verify_change(file_location, "channel", "r_stage_temp_actual", "scaleC", new_value)
+    print verify_change(file_location, "channel", "r_adc1_ain7", "scaleC", new_value)
 
-# validate change?
 
-#call(["systemctl", "restart", "mk5.service"])
+    call(["systemctl", "restart", "mk5.service"])
 
 if __name__ == "__main__":
     main()

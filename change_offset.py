81e32abf783ebfc8ff45cbc9df7d36bcff92be3f
scharczynski <scharczynski@gmail.com>
2017-11-03 16:11:27 -0400

    added message

--- change_offset.py
+++ change_offset.py
@@ -22,7 +22,7 @@ def main():
     call(["systemctl", "restart", "mk5.service"])
 
 
-
+    print "Restarting MK5 service"
     for n in xrange(15):
         time.sleep(1)
         sys.stdout.write("#")

2d33658f5de7a23650e484d0d45ea23dde0625e3
scharczynski <scharczynski@gmail.com>
2017-11-13 15:21:16 -0500

    changed script to increment offset

--- change_offset.py
+++ change_offset.py
@@ -1,4 +1,4 @@
-from xml_changer import change_element, verify_change
+from xml_changer import change_element, verify_change, increment_element, verify_increment
 from subprocess import call
 import time
 import datetime
@@ -14,10 +14,10 @@ def main():
     call(['cp', '/opt/ht/mk5/config/system.xml',
           '/opt/ht/mk5/config/system_' + date_now + str(time_now) + '.xml'])
 
-    change_element(file_location, "channel",
+    past_value = increment_element(file_location, "channel",
                    "r_adc1_ain7", "scaleC", new_value)
-    print verify_change(
-        file_location, "channel", "r_adc1_ain7", "scaleC", new_value)
+    print verify_increment(
+        file_location, "channel", "r_adc1_ain7", "scaleC", new_value, past_value)
 
     call(["systemctl", "restart", "mk5.service"])
 
--- xml_changer.py
+++ xml_changer.py
@@ -24,3 +24,27 @@ def verify_change(file, tag, wire_name, attr_name, intended_value):
     find_string = ".//" + m + tag + "[@name='" + wire_name + "']"
     node = root.findall(find_string)[0]
     return node.get(attr_name) == intended_value
+
+def increment_element(file, tag, wire_name, attr_name, value, output_file=file):
+    
+    etree.register_namespace("", "http://www.ptcusa.com")
+    e = etree.parse(file)
+    root = e.getroot()
+    m = re.match('\{.*\}', root.tag).group(0)
+    find_string = ".//" + m + tag + "[@name='" + wire_name + "']"
+    node = root.findall(find_string)[0]
+    old_val = node.get(attr_name)
+    node.set(attr_name, str(value + old_val))
+
+    e.write(file, encoding="iso-8859-1", xml_declaration=True)
+
+    return old_val
+
+
+def verify_increment(file, tag, wire_name, attr_name, intended_value, old_val):
+    e = etree.parse(file)
+    root = e.getroot()
+    m = re.match('\{.*\}', root.tag).group(0)
+    find_string = ".//" + m + tag + "[@name='" + wire_name + "']"
+    node = root.findall(find_string)[0]
+    return node.get(attr_name) == intended_value + old_val
